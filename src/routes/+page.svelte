fd09ac8dac3e13fd201add3fad8b92755450ec6f
Alexander Poole <alexanderp@opera.com>
2024-07-07 19:49:20 +0200

    Use @owodunni/bricks-ui as component library

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,31 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+	import { type PageSeoMetaData, PageSeo } from '@owodunni/bricks-ui';
+
+	const seoMeta: PageSeoMetaData = {
+		name: 'hello',
+		description: '',
+		url: 'https://hello.se',
+		ldImage: {
+			name: '',
+			url: 'https://hello.se',
+			width: 600,
+			height: 315
+		},
+		ogImage: {
+			name: '',
+			url: 'https://hello.se',
+			width: 1920,
+			height: 1080
+		},
+		organization: {
+			name: '',
+			url: 'https://hello.se',
+			email: 'test@email.se',
+			legalName: ''
+		}
+	};
+</script>
+
+<PageSeo seo={seoMeta} />
+
+<h1>Mätarolle.se</h1>
